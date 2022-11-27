9543d585f8b03f8068e50518eefe230035dc1c33
Denys Zaitsev <denzai83@gmail.com>
2022-11-27 14:47:35 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -7,7 +7,7 @@ import sys
 def normalize(name):
 
     CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
-    TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+    TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "zh", "z", "y", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
                    "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
 
     TRANS = {}
@@ -18,50 +18,56 @@ def normalize(name):
 
     new_name = name.translate(TRANS)
 
-    new_name = re.sub('[^a-zA-Z0-9 \n\.]', '_', new_name)
+    new_name = re.sub("[^a-zA-Z0-9 \n\.]", "_", new_name)
 
     return new_name
 
 
-basepath = os.path.abspath(sys.argv[1])
+if __name__ == "__main__":
 
-img_ext = ['.jpeg', '.png', '.jpg', '.svg']
-doc_ext = ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx']
-aud_ext = ['.mp3', '.ogg', '.wav', '.amr']
-vid_ext = ['.avi', '.mp4', '.mov', '.mkv']
-arc_ext = ['.zip', '.gz', '.tar']
+    basepath = os.path.abspath(sys.argv[1])
 
-if not os.path.exists(basepath + '/Images'):
-    os.mkdir(basepath + '/Images')
-if not os.path.exists(basepath + '/Documents'):
-    os.mkdir(basepath + '/Documents')
-if not os.path.exists(basepath + '/Audio'):
-    os.mkdir(basepath + '/Audio')
-if not os.path.exists(basepath + '/Video'):
-    os.mkdir(basepath + '/Video')
-if not os.path.exists(basepath + '/Archives'):
-    os.mkdir(basepath + '/Archives')
+    CATEGORIES = {
+        "images": [".jpeg", ".png", ".jpg", ".svg"],
+        "documents": [".doc", ".docx", ".txt", ".pdf", ".xlsx", ".pptx"],
+        "audio": [".mp3", ".ogg", ".wav", ".amr"],
+        "video": [".avi", ".mp4", ".mov", ".mkv"],
+        "archives": [".zip", ".gz", ".tar"]
+    }
 
-for root, dirs, files in os.walk(basepath, topdown=False):
-    for dir in dirs:
-        os.rename(os.path.join(root, dir), os.path.join(root, normalize(dir)))
+    for keys in list(CATEGORIES):
+        target = f"{basepath}/{keys.capitalize()}"
+        if not os.path.exists(target):
+            os.mkdir(target)
 
-for root, dirs, files in os.walk(basepath):
-    for file in files:
-        if os.path.splitext(file)[1].lower() in img_ext:
-            os.replace(f'{root}/{file}',
-                       f'{basepath}/Images/{normalize(file)}')
-        elif os.path.splitext(file)[1].lower() in doc_ext:
-            os.replace(f'{root}/{file}',
-                       f'{basepath}/Documents/{normalize(file)}')
-        elif os.path.splitext(file)[1].lower() in aud_ext:
-            os.replace(f'{root}/{file}', f'{basepath}/Audio/{normalize(file)}')
-        elif os.path.splitext(file)[1].lower() in vid_ext:
-            os.replace(f'{root}/{file}', f'{basepath}/Video/{normalize(file)}')
-        elif os.path.splitext(file)[1].lower() in arc_ext:
-            shutil.unpack_archive(
-                f'{root}/{file}', f'{basepath}/Archives/{os.path.splitext(normalize(file))[0]}')
+    for root, dirs, files in os.walk(basepath, topdown=False):
+        for dir in dirs:
+            os.rename(os.path.join(root, dir),
+                      os.path.join(root, normalize(dir)))
 
-for root, dirs, files in os.walk(basepath, topdown=False):
-    if not dirs and not files:
-        os.rmdir(root)
+    for root, dirs, files in os.walk(basepath):
+        for file in files:
+            if os.path.splitext(file)[1].lower() in CATEGORIES["images"]:
+                os.replace(f"{root}/{file}",
+                           f"{basepath}/Images/{normalize(file)}")
+            elif os.path.splitext(file)[1].lower() in CATEGORIES["documents"]:
+                os.replace(f"{root}/{file}",
+                           f"{basepath}/Documents/{normalize(file)}")
+            elif os.path.splitext(file)[1].lower() in CATEGORIES["audio"]:
+                os.replace(f"{root}/{file}",
+                           f"{basepath}/Audio/{normalize(file)}")
+            elif os.path.splitext(file)[1].lower() in CATEGORIES["video"]:
+                os.replace(f"{root}/{file}",
+                           f"{basepath}/Video/{normalize(file)}")
+            elif os.path.splitext(file)[1].lower() in CATEGORIES["archives"]:
+                os.replace(f"{root}/{file}",
+                           f"{basepath}/Archives/{normalize(file)}")
+                shutil.unpack_archive(f"{basepath}/Archives/{file}",
+                                      f"{basepath}/Archives/{os.path.splitext(file)[0]}")
+
+    for root, dirs, files in os.walk(basepath, topdown=False):
+        if not files:
+            try:
+                os.rmdir(root)
+            finally:
+                continue
